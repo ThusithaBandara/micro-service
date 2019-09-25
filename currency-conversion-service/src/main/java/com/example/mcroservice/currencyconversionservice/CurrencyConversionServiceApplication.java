2f9aeca2e1ed437b84451b6d6acad514160b4335
ThusithaBandara <thusitharmtgk95@gmail.com>
2019-09-25 11:28:59 +0530

    Connect services to Eureka naming Server

--- currency-conversion-service/src/main/java/com/example/mcroservice/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/example/mcroservice/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,11 +2,14 @@ package com.example.mcroservice.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.example.mcroservice.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
